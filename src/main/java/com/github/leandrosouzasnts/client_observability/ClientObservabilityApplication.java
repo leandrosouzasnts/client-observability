b95dff93ee1825263d081551b3e6980b8855dc3f
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2024-09-28 11:22:11 -0300

    initial commit

---
+++ src/main/java/com/github/leandrosouzasnts/client_observability/ClientObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.github.leandrosouzasnts.client_observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientObservabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leandrosouzasnts/client_observability/ClientObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandrosouzasnts.client_observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
